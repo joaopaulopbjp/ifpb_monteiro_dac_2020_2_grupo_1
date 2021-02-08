b1644165d88efb4ff474fa8da58c67e0e93e3531
Arkan <Arkan@DESKTOP-H8LGR9T>
2021-02-08 16:08:49 -0300

    novas classe adicionada em model relacionadas ao Carrinho

--- src/main/java/br/edu/ifpb/dac/springdata/model/Order.java
+++ src/main/java/br/edu/ifpb/dac/springdata/model/Order.java
@@ -5,20 +5,18 @@ package br.edu.ifpb.dac.springdata.model;
  * @author: Gabriel Oliveira && Alisson
  */
 
-import java.util.List;
 
-import javax.persistence.CascadeType;
+
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
+
 import javax.persistence.Table;
-import javax.persistence.JoinColumn;
+
 
 @Entity
 @Table(name = "order_tb")
